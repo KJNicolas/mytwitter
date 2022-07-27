58df18f64141206c58cfded686ea6310fb63f571
Freedman J Nicolas <100964607+KJNicolas@users.noreply.github.com>
2022-07-27 12:49:42 -0700

    commit to Master branch

--- schemas/UserSchema.js
+++ schemas/UserSchema.js
@@ -1,9 +1,8 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema(
-  {
+const UserSchema = new Schema({
     firstName: { type: String, required: true, trim: true },
     lastName: { type: String, required: true, trim: true },
     username: { type: String, required: true, trim: true, unique: true },
@@ -11,13 +10,11 @@ const UserSchema = new Schema(
     password: { type: String, required: true },
     profilePic: { type: String, default: "/images/profilePic.jpeg" },
     coverPhoto: { type: String },
-    likes: [{ type: Schema.Types.ObjectId, ref: "Post" }],
-    retweets: [{ type: Schema.Types.ObjectId, ref: "Post" }],
-    following: [{ type: Schema.Types.ObjectId, ref: "User" }],
-    followers: [{ type: Schema.Types.ObjectId, ref: "User" }],
-  },
-  { timestamps: true }
-);
+    likes: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
+    retweets: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
+    following: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    followers: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+}, { timestamps: true });
 
-var User = mongoose.model("User", UserSchema);
+var User = mongoose.model('User', UserSchema);
 module.exports = User;
