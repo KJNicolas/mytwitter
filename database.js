41677fabd23c4a515f6df5f525d2c86e2b1190dc
Freedman J Nicolas <100964607+KJNicolas@users.noreply.github.com>
2022-08-14 09:18:45 -0700

    Update database.js

--- database.js
+++ database.js
@@ -5,10 +5,10 @@
 //created and returned to you the first time because of how module
 //import/export works in ES6.
 const mongoose = require("mongoose");
-mongoose.set("useNewUrlParser", true);
+/* mongoose.set("useNewUrlParser", true);
 mongoose.set("useUnifiedTopology", true);
 mongoose.set("useFindAndModify", false);
-mongoose.set("useUnifiedTopology", true);
+mongoose.set("useUnifiedTopology", true); */
 
 class Database {
   constructor() {
